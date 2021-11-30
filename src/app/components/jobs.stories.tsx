4da4a3a08e064948e76f7dc91cfb024ff3a5120a
Amir Arad <greenshade@gmail.com>
2021-12-01 00:38:39 +0200

    refactor jobs stories

--- src/app/components/jobs.stories.tsx
+++ src/app/components/jobs.stories.tsx
@@ -2,10 +2,10 @@
 /** @jsx jsx */
 
 import { Jobs, JobsProps } from './jobs';
+import { Meta, StoryFn } from '@storybook/react';
 import { ids, idsFromMap, stateBuilder } from '../model';
 
 import { AppStateDecorator } from '../hooks/app-state';
-import { Meta } from '@storybook/react';
 import { StyleDecorator } from '../style';
 import { jsx } from '@emotion/react';
 
@@ -31,25 +31,36 @@ export default {
     title: 'Component/Jobs',
     component: Jobs,
     decorators: [AppStateDecorator(state), StyleDecorator],
-    args: {
-        trainings: idsFromMap(state.trainings),
-        programExecutions: idsFromMap(state.programExecutions),
-        plannings: idsFromMap(state.plannings),
-    } as JobsProps,
+    args: {},
 } as Meta;
 
-export const Primary = (args: JobsProps) => <Jobs {...args} />;
-export const ManyValues = (args: JobsProps) => (
-    <Jobs
-        trainings={Array(10)
-            .fill(args.trainings)
-            .flatMap((i) => i)}
-        programExecutions={Array(10)
-            .fill(args.programExecutions)
-            .flatMap((i) => i)}
-        plannings={Array(10)
-            .fill(args.plannings)
-            .flatMap((i) => i)}
-    />
-);
-export const Empty = () => <Jobs />;
+const args = {
+    trainings: idsFromMap(state.trainings),
+    programExecutions: idsFromMap(state.programExecutions),
+    plannings: idsFromMap(state.plannings),
+};
+
+const Template: StoryFn<JobsProps> = (args: JobsProps) => <Jobs {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = { ...args };
+
+export const ManyValues = Template.bind({});
+ManyValues.args = {
+    trainings: Array(10)
+        .fill(args.trainings)
+        .flatMap((i) => i),
+    programExecutions: Array(10)
+        .fill(args.programExecutions)
+        .flatMap((i) => i),
+    plannings: Array(10)
+        .fill(args.plannings)
+        .flatMap((i) => i),
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+    trainings: [],
+    programExecutions: [],
+    plannings: [],
+};
